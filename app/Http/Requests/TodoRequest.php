26c74df3c9d35daa6359216eb2b3a2c9e686d81a
Takato Kamikiya <kaosu763@gmail.com>
2026-04-09 14:48:52 +0900

    Todo初級の追加

---
+++ app/Http/Requests/TodoRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'content' => ['required', 'string', 'max:20']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'content.required' => 'Todoを入力してください',
+            'content.string' => 'Todoを文字列で入力してください',
+            'content.max' => 'Todoを20文字以下で入力してください',
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
